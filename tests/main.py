390bc6bd01ad67fda72d1bce758fc567005444fc
joaobenedetmachado <jvbm2177@gmail.com>
2026-02-15 20:08:45 -0300

    a really bad way to implement history chat, but works

--- tests/main.py
+++ tests/main.py
@@ -11,27 +11,49 @@ from langchain_core.tools import tool
 
 name = "Joao Victor"
 
+db = {}
+
 
 @tool
 def get_name() -> str:
     """Returns the name of the person. Use when the user asks for the name or who they are."""
     return name
 
+@tool
+def get_history() -> str:
+    """Returns the last messages between the agent and the user"""
+    return db
 
 agent = create_agent(
     model="claude-sonnet-4-5-20250929",
-    tools=[get_name],
+    tools=[get_history],
     system_prompt="You are a helpful assistant.",
 )
 
-result = agent.invoke({
-    "messages": [{"role": "user", "content": "What is the name?"}]
-})
 
-messages = result.get("messages", [])
-if messages:
-    last = messages[-1]
-    if hasattr(last, "content") and last.content:
-        print(last.content)
-    else:
-        print(result)
+def create_message(text):
+    result = agent.invoke({
+    "messages": [{"role": "user", "content": f"{text}"}]
+    })
+
+    messages = result.get("messages", [])
+    if messages:
+        last = messages[-1]
+        if hasattr(last, "content") and last.content:
+            db[len(db.items())]= [f'"{text}" : "{last.content}"']
+
+            print(last.content)
+        else:
+            db[len(db.items())]= [f'"{text}" : "{result}"']
+            print(result)
+
+
+
+
+
+
+while True:
+    message = input("> ")
+    create_message(message)
+
+    
